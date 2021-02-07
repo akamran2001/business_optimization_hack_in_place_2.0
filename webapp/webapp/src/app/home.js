b964fa36ce6132ff6e47213bf2c9cce238d91d8c
Ahmed Muyen Kamran <akamran@colgate.edu>
2021-02-06 19:13:18 -0500

    init react graph

--- webapp/webapp/src/app/home.js
+++ webapp/webapp/src/app/home.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Money_Graph from "./money_graph";
 function home(props) {
   return (
     <div>
       <h1>Bis Viz</h1>
+      <Money_Graph />
     </div>
   );
 }

1dc77161cebeb9cf3cf52c73c3604b3aa8ea1cd4
Parv17k <khatriparv@gmail.com>
2021-02-07 15:38:41 -0500

    hello

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from bson.json_util import dumps
 import json
+from flask import request
 from pymongo import MongoClient
 client = MongoClient("mongodb+srv://business_app:business_app@parvcluster.zg4ux.gcp.mongodb.net/ParvCluster?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
 db = client["business_data"]
@@ -12,6 +13,15 @@ def index():
 
 
 @app.route('/getdata')
+def getdata():
+    data=[]
+    letval=col.find()
+    for i in letval:
+        data.append(i)
+    print(data)
+    return dumps(data)
+
+@app.route('/postdata')
 def getdata():
     data=[]
     letval=col.find()

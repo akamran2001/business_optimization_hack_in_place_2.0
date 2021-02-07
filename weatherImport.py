daa591d44ccd16ca22fd513613fe4d44fde00aa1
JayWoo2000 <joewoo2000@gmail.com>
2021-02-08 02:16:11 +0900

    Update weatherImport.py

--- weatherImport.py
+++ weatherImport.py
@@ -1,21 +1,23 @@
 # Retrieves data from the meteomatics_weather_api using a temporary login credentials
 # and returns the data in a json file. Must install "pip3 install meteomatics"
+# Output is a json file with date as the key for dictionaries and the parameters of air temperature and precipitation.
 
-import meteomatics_weather_api as api
+import meteomatics.api as api
 import datetime as dt
-from pandas import DataFrame
 
-username = hackathon_woo
-password = cJkn4h5qPW7FU
+username = "hackathon_woo"
+password = "cJkn4h5qPW7FU"
 lat = 40.66128977514189
 long = -73.72666306048833
+coords = ((lat, long,),)
 
-startdate = dt.datetime.utc
-enddate = startdate + dt.timedelta(days=90)
-interval = dt.timedelta(hours=1)
-parameters = ['air_temperature', 'relative_humidity', 'precipitation_amount_3h', 'wind_speed']
+startdate = dt.datetime.utcnow().replace(hour=0, minute=0, second=0)
+enddate = startdate + dt.timedelta(days=30)
+print(startdate)
+print(enddate)
+interval = dt.timedelta(days=1)
+parameters = ['t_2m:F', 'precip_24h:mm']
 
-df = api.query_time_series(lat,long,startdate,enddate,interval,parameters, username, password)
+df = api.query_time_series(coords, startdate, enddate,interval,parameters, username, password)
 df.to_json('weatherData.json')
 
-print(response.text)

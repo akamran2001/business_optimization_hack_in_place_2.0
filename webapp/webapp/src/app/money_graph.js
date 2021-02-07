1de28a7332f352c7c0251b1b83f2549534b0bfb8
Ahmed Muyen Kamran <akamran@colgate.edu>
2021-02-07 02:28:01 -0500

    graph of date and price

--- webapp/webapp/src/app/money_graph.js
+++ webapp/webapp/src/app/money_graph.js
@@ -2,25 +2,35 @@ import React from "react";
 import { Chart } from "react-charts";
 import Data from "../data.json";
 function Money_Graph() {
+  let date_price = Data.map((item) => {
+    let currency = item["Purchase Price"];
+    let price = currency.slice(1, currency.length);
+    price = parseFloat(price);
+    const date = new Date(item.DateTime);
+    return {
+      x: date,
+      y: price,
+    };
+  });
+  date_price = date_price.slice(15, 25);
   const data = React.useMemo(
     () => [
       {
-        label: "Series 1",
-        data: [
-          [0, 1],
-          [1, 2],
-          [2, 4],
-          [3, 2],
-          [4, 7],
-        ],
+        label: "Money Graph",
+        data: date_price,
       },
     ],
     []
   );
-
+  const series = React.useMemo(
+    () => ({
+      type: "bar",
+    }),
+    []
+  );
   const axes = React.useMemo(
     () => [
-      { primary: true, type: "linear", position: "bottom" },
+      { primary: true, type: "time", position: "bottom" },
       { type: "linear", position: "left" },
     ],
     []
@@ -31,11 +41,12 @@ function Money_Graph() {
     // space of its parent element automatically
     <div
       style={{
-        width: "400px",
-        height: "300px",
+        width: "700px",
+        height: "700px",
+        background: "rgba(0, 27, 45, 0.9)",
       }}
     >
-      <Chart data={data} axes={axes} />
+      <Chart data={data} axes={axes} series={series} tooltip dark />
     </div>
   );
   return lineChart;

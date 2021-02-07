5ad7616b1441adf087db166a0fce827319e437da
Parv17k <khatriparv@gmail.com>
2021-02-06 23:26:26 -0500

    get api

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,9 +1,21 @@
 from app import app
+from bson.json_util import dumps
+import json
 from pymongo import MongoClient
-client = MongoClient("mongodb+srv://business_app:business_app@parvcluster.zg4ux.gcp.mongodb.net/ParvCluster?retryWrites=true&w=majority")
-db = client.test;
-print(db.collection_names);
+client = MongoClient("mongodb+srv://business_app:business_app@parvcluster.zg4ux.gcp.mongodb.net/ParvCluster?retryWrites=true&w=majority&ssl=true&ssl_cert_reqs=CERT_NONE")
+db = client["business_data"]
+col=db[ "sampledata" ]
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+
+@app.route('/getdata')
+def getdata():
+    data=[]
+    letval=col.find()
+    for i in letval:
+        data.append(i)
+    print(data)
+    return dumps(data)

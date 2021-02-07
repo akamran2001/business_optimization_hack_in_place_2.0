43237d30b4bdaebaea0a24cd5541ebff9f62d7a7
Parv17k <khatriparv@gmail.com>
2021-02-06 22:07:12 -0500

    Mongodb connectivity done

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,8 @@
 from app import app
-
+from pymongo import MongoClient
+client = MongoClient("mongodb+srv://business_app:business_app@parvcluster.zg4ux.gcp.mongodb.net/ParvCluster?retryWrites=true&w=majority")
+db = client.test;
+print(db.collection_names);
 @app.route('/')
 @app.route('/index')
 def index():
